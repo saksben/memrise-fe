5c8953024f4e16890e1cd9e0e80851375228c1b3
Benjamin Saks <saksben2@gmail.com>
2024-11-16 14:32:32 -0700

    connect to backend

--- src/app/course/[courseId]/[course]/edit/layout.tsx
+++ src/app/course/[courseId]/[course]/edit/layout.tsx
@@ -1,22 +1,39 @@
-import Link from "next/link";
+"use client";
 
-const course = {
-  id: 1,
-  name: "French 1",
-  slug: "french-1",
-  coursePic: "",
-  numLessons: 2,
-  user: {
-    name: "Memrise",
-    profilePic: "",
-  },
-};
+import { Course, User } from "@/types/models";
+import { getCourse, getUser } from "@/util/api";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import React from "react";
+
+// TODO: course pic, user pic
 
 export default function EditLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const params = useParams();
+
+  React.useEffect(() => {
+    const fetchCourse = async () => {
+      try {
+        const courseData = await getCourse(params.courseId);
+        setCourse(courseData);
+        if (courseData) {
+          const user = await getUser(String(courseData.authorId));
+          setAuthor(user);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchCourse();
+  }, []);
+
+  const [course, setCourse] = React.useState<Course>();
+  const [author, setAuthor] = React.useState<User>();
+
   return (
     <div>
       <div>
@@ -24,19 +41,22 @@ export default function EditLayout({
           <div className="flex gap-4">
             <div className="size-8 rounded-md bg-white p-[1px]">
               <div className="size-full bg-black rounded-md">
-                {course.coursePic}
+                {/* {course.coursePic} */}
               </div>
             </div>
-            <h1 className="text-2xl font-bold">{course.name}</h1>
+            <h1 className="text-2xl font-bold">{course?.name}</h1>
           </div>
           <div className="flex items-center gap-3">
             <div className="size-10 bg-white rounded p-0.5">
               <div className="size-full bg-black rounded">
-                {course.user.profilePic}
+                {/* {course.user.profilePic} */}
               </div>
             </div>
-            <div className='text-xs text-neutral-300 flex flex-col text-center'>
-              Created by <span className='text-sm font-bold text-white'>{course.user.name}</span>
+            <div className="text-xs text-neutral-300 flex flex-col text-center">
+              Created by{" "}
+              <span className="text-sm font-bold text-white">
+                {author?.username}
+              </span>
             </div>
           </div>
         </section>
@@ -44,11 +64,13 @@ export default function EditLayout({
           <div className="flex gap-2 text-sm items-center">
             <h2 className="text-xl font-bold">Editing</h2>
             <Link
-              href={`/course/${course.id}/${course.slug}/edit`}
+              href={`/course/${course?.id}/${course?.slug}/edit`}
               className="py-2 px-4 hover:bg-slate-300 transition-colors transform duration-300 rounded-full"
-            >{`Levels (${course.numLessons})`}</Link>
+            >
+              {`Levels (${course?.lessons.length})`}
+            </Link>
             <Link
-              href={`/course/${course.id}/${course.slug}/edit/details`}
+              href={`/course/${course?.id}/${course?.slug}/edit/details`}
               className="py-2 px-4 hover:bg-slate-300 transition-colors transform duration-300 rounded-full"
             >
               Details
